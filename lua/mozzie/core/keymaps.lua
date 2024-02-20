3036e6ab01158b9d71636e887d950a77c00c8c93
Mozzie <mozzie@mozzie.local>
2024-02-19 21:11:17 -0600

    set up alpha

--- lua/mozzie/core/keymaps.lua
+++ lua/mozzie/core/keymaps.lua
@@ -1,9 +1,6 @@
 -- keymaps
-local new_keymap = function(mode, new, old)
-  vim.api.nvim_set_keymap(mode, new, old,
-      { noremap = true, silent = true })
-end
 
+-- auto-complete
 local auto_complete_keymap = function(mode, new, old)
   vim.keymap.set(mode, new, function()
     if vim.fn.pumvisible() == 1 then return old end
@@ -11,9 +8,12 @@ local auto_complete_keymap = function(mode, new, old)
   end, { expr = true })
 end
 
--- auto-complete
 auto_complete_keymap('i', '<m-j>', '<c-n>')
 auto_complete_keymap('i', '<m-k>', '<c-p>')
 
 -- follow-md-links
 vim.keymap.set('n', '<bs>', ':edit #<cr>', { silent = true })
+
+-- fzf
+vim.keymap.set('n', '<m-f>', ':Files<cr>', { silent = true })
+vim.keymap.set('n', '<m-b>', ':Buffers<cr>', { silent = true })
