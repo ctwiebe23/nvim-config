b09826d474dc73403c4423c5da5ee25c0fa45f43
Mozzie <mozzie@mozzie.local>
2024-03-24 13:23:45 -0500

    created utility and navigation keymaps

--- xkeysnail/keymaps.py
+++ xkeysnail/keymaps.py
@@ -7,9 +7,26 @@ define_modmap({
     Key.RIGHT_ALT: Key.RIGHT_SHIFT
 })
 
-define_keymap(re.compile("Firefox"), {
-    K("C-M-l"): K("C-TAB"),
-    K("C-M-h"): K("C-Shift-TAB"),
-    K("C-M-j"): K("down"),
-    K("C-M-k"): K("up")
-}, "Firefox")
+define_keymap(
+    re.compile("Firefox"),
+    {
+        K("C-l"): K("C-TAB"),
+        K("C-h"): K("C-Shift-TAB"),
+        K("C-j"): K("page_down"),
+        K("C-k"): K("page_up"),
+    },
+    "Firefox",
+)
+
+define_keymap(
+    lambda window : window not in ("Firefox"),
+    {
+        K("M-h"): K("left"),
+        K("M-j"): K("down"),
+        K("M-k"): K("up"),
+        K("M-l"): K("right"),
+        K("M-q"): K("esc"),
+        K("M-BACKSPACE"): K("delete"),
+    },
+    "Navigation & Utility",
+)
